6ff81eaf0d445e3b8d29ba8b211cb03858064fb1
Mala V <malavengat@gmail.com>
2018-08-24 23:30:46 -0400

    Iterations 2, 3 and 4

--- db/migrate/20180814002707_create_comments.rb
+++ db/migrate/20180814002707_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.references :post, foreign_key: true
+      t.references :post, index: true, foreign_key: true
       t.text :body
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
